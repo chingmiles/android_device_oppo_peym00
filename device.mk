c31c6c50def30d74fb7ae207518a0d7f4cd50398
chingmiles <62104163+chingmiles@users.noreply.github.com>
2024-07-29 20:20:56 +0800

    更新 device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,7 @@
 #
 
 LOCAL_PATH := device/oppo/peym00
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/first_stage_ramdisk/fstab.emmc:root/fstab.emmc \
+    $(LOCAL_PATH)/first_stage_ramdisk/fstab.mt6893:root/fstab.mt6893
